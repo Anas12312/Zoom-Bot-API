5b3855c8690b4467f0d54c27ef57657d9e091d1a
anas12312 <anashesham000@gmail.com>
2024-08-11 15:28:06 +0300

    anas

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const init = require('./Services/init')
 const join = require('./Services/join')
+const waitForJoining = require('./Services/waitForJoining')
 
 require('dotenv').config()
 
@@ -7,7 +8,10 @@ require('dotenv').config()
 async function run() {
     
     const { browser, page } = await init()
-    join(browser, page, "https://us05web.zoom.us/j/81317376491?pwd=EmzFJWfS9JNLfqYP3bj9yBWecGr3SG.1")
-    
+    const joined = await join(browser, page, "https://us04web.zoom.us/j/75651662994?pwd=465OSdMtuteYH2f4bNU4u8i9ViOfpZ.1")
+    if(joined) {
+        console.log("Agent joined successfully!")
+        waitForJoining()
+    }
 }
 run()
